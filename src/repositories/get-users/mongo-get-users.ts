2a9476e0ec747e7d9c9e8d7c957b6bdc7ea245b9
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-03-24 14:55:29 -0300

    feat: add id to user Model

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,7 +1,9 @@
+import { MongoClient } from "@/database/mongo";
 import { type User } from "@/models/user";
 import { type IGetUsersRepository } from "@/protocols";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
+  const users = await MongoClient.db.collection('users').find({}).toArray();
   async getUsers(): Promise<User[]> {
     return [{
       name: 'Bertoldo',
