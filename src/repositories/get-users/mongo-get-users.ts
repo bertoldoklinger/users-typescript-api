2c74a5f2a4748e3be7c64c3fd58b0c72fc8ac235
bertoldoklinger <bertoldokbrj@gmail.com>
2023-04-16 21:37:41 -0300

    feat: add MongoCreateUserController

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -6,9 +6,6 @@ export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
     const users = await MongoClient.db.collection<Omit<User, "id">>("users").find({}).toArray()
 
-
-
-
     return users.map(({ _id, ...rest }) => ({ ...rest, id: _id.toHexString(), }))
 
   }
